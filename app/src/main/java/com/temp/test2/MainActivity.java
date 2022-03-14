2288350c7c4ae0b6c5e0765282d8c0a25fb67cb2
Akshay Jayale <akshayj@techepoch.com>
2022-03-14 09:54:52 +0530

    changes

--- app/src/main/java/com/temp/test2/MainActivity.java
+++ app/src/main/java/com/temp/test2/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.reactnativecommunity.checkbox.ReactCheckBoxPackage;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new ReactCheckBoxPackage();
+//        ToasterMessage.message(MainActivity.this, "dkhbskd");
     }
 }
