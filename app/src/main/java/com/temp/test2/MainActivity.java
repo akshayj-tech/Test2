da4d040d3b53c7e9dfe5d08cb1442c2de0c01c35
Akshay Jayale <akshayj@techepoch.com>
2022-03-16 01:43:43 +0530

    changes 1.0

--- app/src/main/java/com/temp/test2/MainActivity.java
+++ app/src/main/java/com/temp/test2/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+//import com.temp.mylibrary.ToasterMessage;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
